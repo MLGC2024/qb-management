640ed636b3974798e23ac79279dbe6d8786f48ba
GG79 <midlifegamingcrisis@gmail.com>
2024-02-04 21:00:04 +0200

    changed cardealer coords

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -17,7 +17,10 @@ Config.BossMenus = {
         vector3(894.96, -179.07, 74.7),
     },
     ['cardealer'] = {
-        vector3(-27.47, -1107.13, 27.27),
+        vector3(-1249.77, -345.29, 37.33) -- Luxury
+    },
+    ['cardealer'] = {
+        vector3(-30.51, -1106.69, 26.42)  -- PDM
     },
     ['mechanic'] = {
         vector3(-339.53, -156.44, 44.59),
